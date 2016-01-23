f83ea0ba688f215c1be5c04b5f879a03defc4354
thingsastray <andrewpeterson808@gmail.com>
2016-01-23 11:04:05 -1000

    Phaser load 2

--- public/js/states/game.js
+++ public/js/states/game.js
@@ -2,11 +2,11 @@
 
 kickface.Game = function (){
 
-}
+};
 
 
 kickface.Game.prototype.create = function() {
-	 console.log('Game state create()');
+
 };
 
 kickface.Game.prototype.update = function() {
