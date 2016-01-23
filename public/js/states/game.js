16bbf1ce65709171e95372259682d4fcea5ac973
thingsastray <andrewpeterson808@gmail.com>
2016-01-23 12:56:06 -1000

    add player 2, and position each player

--- public/js/states/game.js
+++ public/js/states/game.js
@@ -1,23 +1,42 @@
-// class constructor
+(function(){
 
-kickface.Game = function (){
+	var INITIAL_POSTIONS = [
+	//player 1
+	{ x:100, y:100 },
+	//player 2
+	{ x: 600, y: 100 }
+	];
+	// class constructor
 
-	this.player_1;
+	kickface.Game = function (){
 
-};
+		this.player_1;
+		this.player_2;
+
+	};
 
 
-kickface.Game.prototype.create = function() {
+	kickface.Game.prototype.create = function() {
 
-	this.player_1 = new kickface.Player( this.game, 0);
-	this.game.add.existing(this.player_1);
+		this.player_1 = new kickface.Player( this.game, 0);
+		this.player_2 = new kickface.Player( this.game, 1);
+		this.game.add.existing(this.player_1);
+		this.game.add.existing(this.player_2);
 
-};
+		//position players
+		this.player_1.x = INITIAL_POSTIONS[0].x;
+		this.player_1.y = INITIAL_POSTIONS[0].y;
+		this.player_2.x = INITIAL_POSTIONS[1].x;
+		this.player_2.y = INITIAL_POSTIONS[1].y;
 
-kickface.Game.prototype.update = function() {
-	
-};
+	};
 
-kickface.Game.prototype.shutdown = function() {
-	
-};
+	kickface.Game.prototype.update = function() {
+		
+	};
+
+	kickface.Game.prototype.shutdown = function() {
+		
+	};
+})();
+
