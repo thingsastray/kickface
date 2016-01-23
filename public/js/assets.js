1b5076dd52ad19afa9af66527abc81af3e645b41
thingsastray <andrewpeterson808@gmail.com>
2016-01-23 12:00:27 -1000

    new

---
+++ public/js/assets.js
@@ -0,0 +1,25 @@
+/*
+  each asset is organized by type
+  each asset has struct
+    {
+      name : String
+      path : String
+      width : Integer @optional
+      height : Integer @optional
+      frames : Integer @optional
+    }
+ */
+
+ kickface.ASSETS = {
+ 	//asset path must be a valid game.load.[x] method
+ 	SPRITESHEET : {
+ 		PLAYER : {
+ 			name : 'player',
+ 			path : 'assets/graphics/player_100x96.png',
+ 			width : 100,
+ 			height : 96,
+ 			frames : 18,
+ 			frames_per_row : 9 // spilt spritesheet by player/row
+ 		}
+ 	}
+ };
--- public/js/states/boot.js
+++ public/js/states/boot.js
@@ -7,6 +7,18 @@ kickface.Boot = function (){
 
 kickface.Boot.prototype.preload = function() {
 	// will preload all assets
+	// 
+	Object.keys(kickface.ASSETS).forEach(function(type){
+		for( var asset in kickface.ASSETS[type]){
+			kickface.game.load[ type.toLowerCase ()](
+				kickface.ASSETS[type][asset].name,
+				kickface.ASSETS[type][asset].path,
+				kickface.ASSETS[type][asset].width,
+				kickface.ASSETS[type][asset].height,
+				kickface.ASSETS[type][asset].frames
+			);
+		}
+	});
 };
 
 kickface.Boot.prototype.create = function() {
